e055de20d12f5d677ead26071b85314a201c0519
Shon Garrison <shon.garrison@yahoo.com>
2020-05-31 16:46:59 -0500

    Repaired bad alias commands. Took out some apps that I find unuseful.

--- menu.sh
+++ menu.sh
@@ -15,7 +15,10 @@ do
   echo ""
   echo "Enter u to run update: "
   echo "Enter c for current calendar: "
-  echo "Enter cl to clean apt-get: "
+  echo "Enter cl to clean apt: "
+  echo ""
+  echo "====================== Scripts ======================"
+  echo ""
   echo "Enter sgc for simple timer < 1 min: "
   echo "Enter sgac for an advanced timer > 1 min: "
   echo ""
@@ -32,12 +35,10 @@ do
   echo "Enter e for elinks browswer: "
   echo "Enter w for weechat: "
   echo "Enter cal for calcurse: "
-  echo "Enter fin for finch instant messenger: "
   echo "Enter ht for htop: "
   echo "Enter mt for mutt email client:"
   echo "Enter sc for spreadsheet calculator: "
   echo "Enter nb for newsbeuter rss feed reader: "
-  echo "Enter tl for timeline: "
   echo ""
   echo "====================================================="
   echo ""
@@ -49,25 +50,23 @@ do
   echo ""
   
   case "$answer" in
-    u) sudo apt-get update && sudo apt-get upgrade ;;
-    r) gcash_restore ;;
-    b) gcash_bak ;;
-    a) gcash_bak_adv ;;
-    f) gcash_bak_fs ;;
+    u) sudo apt update && sudo apt upgrade ;;
+    r) cd ~/Programming/Local/BASH/gCash && ./gnuCash_restore.sh; cd  ;; #gcrest
+    b) cd ~/Programming/Local/BASH/gCash && ./gnuCash_backup.sh; cd ;; #gcbak 
+    a) cd ~/Programming/Local/BASH/gCash && ./gnuCash_backup_adv.sh; cd ;; #gcbadv 
+    f) cd ~/Programming/Local/BASH/gCash && ./gnuCash_backup_fserver.sh; cd ;; #gcbfs 
     c) cal ;;
     cl) sudo apt-get clean && sudo apt-get autoclean && sudo apt-get autoremove ;;
     p) cd ~/Documents/Notes/pygmynote/;./pygmynote.py ;;
     e) elinks ;;
     w) weechat-curses ;;
     cal) calcurse ;;
-    sgac) adv_countdown ;;
-    sgc) countdown ;;
-    fin) finch ;;
+    sgac) cd ~/Programming/Local/BASH/Timers && ./adv_countdown.sh; cd ;; #acount 
+    sgc) cd ~/Programming/Local/BASH/Timers && ./countdown.sh; cd ;; #countd
     ht) htop ;;
     mt) mutt ;;
     sc) sc ;;
     nb) newsbeuter ;;
-    tl) python .timeline-1.6.0/source/timeline.py ;;
     q) exit ;;
   esac
   echo ""
