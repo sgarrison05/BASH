acc19b005fe03e85b8608e92c4e5055d791316a5
Shon Garrison <sgarrison05@sbcglobal.net>
2023-03-16 15:15:23 -0500

    Added Back option to whiptail menu

--- menu/whiptail/whiptail_menu/wt_adv_menu.sh
+++ menu/whiptail/whiptail_menu/wt_adv_menu.sh
@@ -13,33 +13,36 @@ choice=$(whiptail --title "BASH menu" --menu "Choose an option" 25 78 16 \
     "7" "htop" \
     "8" "Spreadsheet Calculator (sc)" \
     "9" "Newsboat" \
+    "<--Back" "Return to Shell" \
     3>&1 1>&2 2>&3) 
 
 if [ -z "$choice" ]; then
     echo "No option was selected (User Hit Cancel)"
 else
     case "$choice" in 
-    "1") sudo nala update && sudo nala upgrade
-       ;;
-    "2") sudo apt clean && sudo apt autoclean && sudo apt autoremove
-       ;;
-    "3") cal
-       ;;
-    "4") countdown
-       ;;
-    "5")  adv_countdown
-       ;;
-    "6") calcurse
-       ;;
-    "7") htop
-       ;;
-    "8") sc
-       ;;
-    "9") newsboat
-       ;;
-    *) echo "Unsupported Item $choice!" >&2
-       exit 1
-       ;;
+        "1") sudo nala update && sudo nala upgrade
+           ;;
+        "2") sudo apt clean && sudo apt autoclean && sudo apt autoremove
+           ;;
+        "3") cal
+           ;;
+        "4") countdown
+           ;;
+        "5")  adv_countdown
+           ;;
+        "6") calcurse
+           ;;
+        "7") htop
+           ;;
+        "8") sc
+           ;;
+        "9") newsboat
+           ;;
+        "<--Back") exit 0
+            ;;
+        *) echo "Unsupported Item $choice!" >&2
+           exit 1
+           ;;
     esac
 fi
 
