8ebf44549676846f80d997f6de74d1a9aab3a7f7
Shon Garrison <sgarrison05@sbcglobal.net>
2023-04-04 12:12:37 -0500

    Clarified files
    Removed old .sh files and added a .txt file with one-liners

--- awk/missing_dispositions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# awk scrpt to print missing dispositions in .csv file from cjis.
-# Original script on the command line
-
-awk -F '\",\"' '{if ($14 == "J" && $23 == "") print $2,$3,$4,$5,$9,$10,$14,$23}' fullCountyOpenAndClosedOffenseReport.csv
-
